cfab5e7842cacc0770a5169ddb4e51be36be41ee
Ben Tupper <btupper@bigelow.org>
2022-06-29 10:28:41 -0400

    make polygon and bbox ready

--- R/xyzt.R
+++ R/xyzt.R
@@ -1,3 +1,24 @@
+#' Get geometry dimension code
+#' 
+#' @export
+#' @param x sf or sfc object
+#' @return character vector such as "XY" or "XYZ"
+get_geometry_dimension <- function(x){
+  x <- sf::st_geometry(x)
+  sort(unique(sapply(x, function(x) class(x)[1])))
+}
+
+#' Get geometry type code
+#' 
+#' @export
+#' @param x sf or sfc object
+#' @return character vector such as "POINT" or "POLYGON"
+get_geometry_type <- function(x){
+  klass <- sf::st_geometry(x) |>
+    class()
+  sub("sfc_", "", klass[1])
+}
+
 #' Convert a data frame (or tibble) to an sf POINT object
 #' 
 #' @export
@@ -47,6 +68,38 @@ as_POINT <- function(x = read_gom(),
     "xyz" = sf::st_as_sf(x, coords = c(ix, iy, iz), dim = "XYZ", crs = crs, ...),
     "xyt" = sf::st_as_sf(x, coords = c(ix, iy, it), dim = "XYM", crs = crs, ...),
     "xyzt" = sf::st_as_sf(x, coords = c(ix, iy, iz, it), dim = "XYZM", crs = crs, ...))
+}
+
+#' Convert a POINTS object to a POLYGON.  Assumes that the points are ordered
+#' correctly and not closed (we'll close it for you.) Note that attributes (variables)
+#' are dropped.
+#' 
+#' @export
+#' @param x POINTS object
+#' @return sf object with one POLYGON record
+#' @seealso \href{https://stackoverflow.com/questions/48383990/convert-sequence-of-longitude-and-latitude-to-polygon-via-sf-in-r}{Stackoverflow discussion}
+as_POLYGON <- function(x){
+  if(!inherits(x, "sf")) x <- as_POINT(x)
+  g <- sf::st_geometry(x)
+  xy <- sf::st_coordinates(g)
+  xy <- sf::st_sfc(sf::st_polygon(list(rbind(xy, xy[1,]))))
+  sf::st_sf(dplyr::tibble(xy), 
+                crs = sf::st_crs(g))
+}
+
+#' Convert any object to a bounding box POLYGON.  
+#' Note that attributes (variables) are dropped.
+#' 
+#' @export
+#' @param x sf object
+#' @return sf object with one POLYGON record
+as_BBOX <- function(x){
+  if(!inherits(x, "sfc")) x <- as_POINT(x)
+  bb <- sf::st_bbox(x)
+  xy <- cbind(bb[c(1,3,3,1,1)], bb[c(2,2,4,4,2)])
+  xy <- sf::st_sfc(sf::st_polygon(list(xy)))
+  sf::st_sf(xy, 
+            crs = sf::st_crs(x))
 }
 
 
